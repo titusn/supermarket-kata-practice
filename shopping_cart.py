8c503c080e98b517db4c46e8775081276bee8532
Titus Nachbauer <mail@titusnachbauer.com>
2022-11-28 09:26:36 +0000

    Update README

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,1 +1,31 @@
 class ShoppingCart:
+    
+    def __init__(self):
+        self.item_list = []
+
+    @property
+    def item_count(self):
+        return len(self.item_list)
+
+
+    def add(self, item):
+        self.item_list.append(item)
+        
+    def remove(self, item):
+        if self.item_count == 0:
+           raise UnderflowError 
+        self.item_list.remove(item)
+
+    def items(self):
+        return self.item_list
+
+    def total_price(self):
+        total_price = 0
+        for item in self.item_list:
+            total_price += item.price
+            
+        return total_price
+
+class UnderflowError(Exception):
+    pass
+
